e115c0edeb69db66a937253e4e638d617c8a937e
ankitk26 <myselfankit51@gmail.com>
2026-03-21 10:41:14 +0530

    switch to pnpm and fix new bugs

--- src/components/UserHeader.tsx
+++ src/components/UserHeader.tsx
@@ -2,22 +2,26 @@ import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 import { useImage } from "../context/ImageContext";
 
 export default function UserHeader() {
-  const { current } = useImage();
+	const { current } = useImage();
 
-  return (
-    <a href={current?.user.links.html} target="_blank" rel="noreferrer">
-      <Flex alignItems="center" gap={4}>
-        <Avatar
-          src={current?.user.profile_image.large}
-          alt={current?.user.name}
-        />
-        <Box display="flex" flexDirection="column" alignItems="flex-start">
-          <Text fontSize="lg">{current?.user.name}</Text>
-          <Text fontSize="md" color="gray.500">
-            @{current?.user.username}
-          </Text>
-        </Box>
-      </Flex>
-    </a>
-  );
+	return (
+		<a href={current?.user.links.html} target="_blank" rel="noreferrer">
+			<Flex alignItems="center" gap={4}>
+				<Avatar
+					src={current?.user.profile_image.large}
+					// alt={current?.user.name}
+				/>
+				<Box
+					display="flex"
+					flexDirection="column"
+					alignItems="flex-start"
+				>
+					<Text fontSize="lg">{current?.user.name}</Text>
+					<Text fontSize="md" color="gray.500">
+						@{current?.user.username}
+					</Text>
+				</Box>
+			</Flex>
+		</a>
+	);
 }
--- src/pages/ImageDetails.tsx
+++ src/pages/ImageDetails.tsx
@@ -5,35 +5,35 @@ import ImageInfoHeader from "../components/ImageInfoHeader";
 import { useImage } from "../context/ImageContext";
 
 export default function ImageDetails() {
-  const params = useParams();
-  const id = params.id;
+	const params = useParams();
+	const id = params.id;
 
-  const { current, loading, clearCurrent, fetchImageById } = useImage();
+	const { current, loading, clearCurrent, fetchImageById } = useImage();
 
-  useEffect(() => {
-    if (id) {
-      fetchImageById(id);
-    }
-    return () => {
-      clearCurrent();
-    };
-    // eslint-disable-next-line
-  }, []);
+	useEffect(() => {
+		if (id) {
+			fetchImageById(id);
+		}
+		return () => {
+			clearCurrent();
+		};
+		// eslint-disable-next-line
+	}, []);
 
-  if (loading) {
-    return <Spinner size="md" />;
-  }
+	if (loading) {
+		return <Spinner size="md" />;
+	}
 
-  return (
-    <Box mb={5}>
-      <ImageInfoHeader />
-      <Flex justifyContent="center" my={8}>
-        <Image src={current?.urls.small} alt={current?.id} />
-      </Flex>
-      <Box mt={2} />
-      <Text fontSize="lg" color="gray.600">
-        {current?.description}
-      </Text>
-    </Box>
-  );
+	return (
+		<Box mb={5}>
+			<ImageInfoHeader />
+			<Flex justifyContent="center" my={8}>
+				<Image src={current?.urls.small} alt={current?.id} />
+			</Flex>
+			<Box mt={2} />
+			<Text fontSize="lg" color="gray.600">
+				{current?.description}
+			</Text>
+		</Box>
+	);
 }
