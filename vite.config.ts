339eb0d0fd7fb0b0d92a77faa7a9808b24ca4d71
ankitk26 <myselfankit51@gmail.com>
2026-03-21 14:05:32 +0530

    vite plus

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import path from "path";
+import { fileURLToPath } from "url";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from "vite-plus";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+	fmt: {
+		ignorePatterns: [],
+		useTabs: true,
+		tabWidth: 4,
+		printWidth: 80,
+		sortImports: {
+			newlinesBetween: false,
+		},
+	},
 	plugins: [react()],
 	resolve: {
 		alias: {
